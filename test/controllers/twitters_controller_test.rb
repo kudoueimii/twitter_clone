bee405b29888d1f49d9767d9e672638d5952eada
bargholz eimii <818_77_@ezweb.ne.jp>
2022-09-27 17:02:58 +0900

    Add creating fanction and redirection fanction

---
+++ test/controllers/twitters_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class TwittersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get twitters_index_url
+    assert_response :success
+  end
+end
