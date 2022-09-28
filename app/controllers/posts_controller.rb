aef9f5135ca77c4abca2f257c1352ccca267bd71
bargholz eimii <818_77_@ezweb.ne.jp>
2022-09-28 14:39:26 +0900

    Add edit function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create(post_params)
-    Post.create()
-    redirect_to new_post_path
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "Created!"
+    else
+      render :new
+    end  
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Edited!"
+    else
+      render :edit    
+   end
   end
 
   private
